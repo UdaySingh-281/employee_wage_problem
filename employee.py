c88c14697bf616acdea9eb3f5f8fab7cad14e5f7
UdaySingh-281 <udaysinghspn28122002@gmail.com>
2025-01-08 12:20:38 +0530

    Added uc3

--- employee.py
+++ employee.py
@@ -1,10 +1,11 @@
 import random
 class Employee:
     print("______Welcome Employee Wage Problem_______")
-    def __init__(self, name, attendance):
+    def __init__(self, name, attendance, type_emp):
         self.name = name
         self.attendance = attendance
         self.daily_wage = None
+        self.type_emp = type_emp
     
     def check_attendance(self):
         if self.attendance:
@@ -12,9 +13,12 @@ class Employee:
         return f"{self.name} is ABSENT"
     
     def calculate(self):
-        self.daily_wage = 20 * 8
+        if self.type_emp.lower() == "part":
+            self.daily_wage = 15 * 8
+        else: 
+            self.daily_wage = 20 * 8
         return f"{self.name} earns {self.daily_wage}rs. daily"
     
-emp1 = Employee("John", random.randint(0,1))
+emp1 = Employee("John", random.randint(0,1), "Part")
 print(emp1.check_attendance())
 print(emp1.calculate())
