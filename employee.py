b70bf98ea9d4e039d963125eb4ea0fe62ebb8a7f
UdaySingh-281 <udaysinghspn28122002@gmail.com>
2025-01-08 12:41:04 +0530

    Added uc6

--- employee.py
+++ employee.py
@@ -1,10 +1,12 @@
 import random
 class Employee:
     print("______Welcome Employee Wage Problem_______")
-    def __init__(self, name, attendance, type_emp):
+    def __init__(self, name, attendance, type_emp, hrs, days):
         self.name = name
         self.attendance = attendance
-        self.daily_wage = None
+        self.wage = None
+        self.hours = hrs
+        self.days = days
         self.type_emp = type_emp
         self.working_days = 20
     
@@ -15,15 +17,24 @@ class Employee:
     
     def calculate(self):
         if self.type_emp.lower() == "part":
-            self.daily_wage = 15 * 8
-        else: 
-            self.daily_wage = 20 * 8
-        return f"{self.name} earns {self.daily_wage}rs. daily"
+            if self.days <= 20 and self.hours <= 100:
+                self.wage = self.hours * self.days * 15
+                daily_wage = self.hours * 15
+            else:
+                return "Not Eligible"
+        else:
+            if self.days <= 20 and self.hours <= 100:
+                self.wage = self.hours * self.days * 20
+                daily_wage = self.hours * 20
+            else:
+                return "Not Eligible"
+            
+        return f"{self.name} earns {daily_wage}rs. daily"
     
     def monthly_wages(self):
-        return f"{self.name} has monthly salary of {(self.daily_wage * self.working_days)}"
+        return f"{self.name} has monthly salary of {(self.wage)}"
     
-emp1 = Employee("John", random.randint(0,1), "Part")
+emp1 = Employee("John", random.randint(0,1), "Part", 100, 20)
 print(emp1.check_attendance())
 print(emp1.calculate())
 print(emp1.monthly_wages())
