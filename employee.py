56df389eec2aa6b8cd7f4d719af75852f7a5cd99
UdaySingh-281 <udaysinghspn28122002@gmail.com>
2025-01-08 12:15:02 +0530

    Added uc2

--- employee.py
+++ employee.py
@@ -4,11 +4,17 @@ class Employee:
     def __init__(self, name, attendance):
         self.name = name
         self.attendance = attendance
+        self.daily_wage = None
     
     def check_attendance(self):
         if self.attendance:
             return f"{self.name} is PRESENT"
         return f"{self.name} is ABSENT"
     
+    def calculate(self):
+        self.daily_wage = 20 * 8
+        return f"{self.name} earns {self.daily_wage}rs. daily"
+    
 emp1 = Employee("John", random.randint(0,1))
 print(emp1.check_attendance())
+print(emp1.calculate())
